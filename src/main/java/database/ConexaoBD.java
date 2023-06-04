94e649a2297f21b15194c295ef21d6ad2fadd737
Felipe Couto <felipdeveloper@gmail.com>
2023-06-04 14:37:20 -0300

    Arquivo de Conexão com Banco de dados

---
+++ src/main/java/database/ConexaoBD.java
@@ -0,0 +1,21 @@
+package database;
+
+import java.sql.*;
+
+public class ConexaoBD {
+
+    private static String host = "localhost";
+    private static String porta = "3306";
+    private static String db = "db_usuario";
+    private static String usuario = "root";
+    private static String senha = "root";
+
+    public static java.sql.Connection obtemConexao() {
+        try {
+            java.sql.Connection c = DriverManager.getConnection("jdbc:mysql://" + host + ":" + porta + "/" + db + "?useTimezone=true&serverTimezone=UTC", usuario, senha);
+            return c;
+        } catch (SQLException e) {
+            return null;
+        }
+    }
+}
